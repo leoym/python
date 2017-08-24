124eae9242104e1847ce50a9f35c60660326b841
Leonardo <leoyasu@yahoo.com.br>
2017-08-24 10:54:13 -0300

    Arguments

---
+++ arguments.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import sys
+
+print '##################################################################'
+print '#  PYTHON                                                        #'
+print '##################################################################'
+print 'Number of arguments:', len(sys.argv), 'arguments.'
+print 'Argument List:', str(sys.argv)
+
+
+
